e159a82eec0d4e75419fbb155166b5b3ab1554dc
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2018-09-26 11:50:16 -0400

    Added getStory action to the StoryModel store

--- src/stores/Story.js
+++ src/stores/Story.js
@@ -1,4 +1,6 @@
-import { types } from 'mobx-state-tree'
+import { types, flow } from 'mobx-state-tree'
+import { client } from '../services/Client'
+import StoryByIdQuery from '../queries/storyById'
 
 const StoryModel = types
   .model('StoryModel', {
@@ -9,10 +11,28 @@ const StoryModel = types
     profileId: types.maybeNull(types.string),
     title: types.maybe(types.string),
   })
-  .actions(self => ({
-    changeTitle(newTitle) {
+  .actions((self) => {
+    const changeTitle = (newTitle) => {
       self.title = newTitle
-    },
-  }))
+    }
+
+    const getStory = flow(function* (storyId) {
+      console.log(storyId)
+      const { story } = yield client.query({
+        query: StoryByIdQuery,
+        variables: ({ storyId })
+      })
+      console.log(story)
+      self.title = story.title;
+      self.description = story.description;
+      self.content = story.content;
+      self.profileId = story.profileId;
+    })
+
+    return {
+      changeTitle,
+      getStory
+    }
+  })
 
 export default StoryModel
