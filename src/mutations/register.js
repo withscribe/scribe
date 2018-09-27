13aee99f11d8633f408ef9f8b92511be90581639
Evan Kysley <evan@kysley.com>
2018-09-27 15:08:06 -0400

    Fix: Regression with API naming

--- src/mutations/register.js
+++ src/mutations/register.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag'
 
 const registerMutation = gql`
-  mutation registerAccountWithProfile($userName: String!, $email: String!, $password: String!) {
-    registerAccountWithProfile(userName: $userName, email: $email, password: $password) {
+  mutation register($userName: String!, $email: String!, $password: String!) {
+    register(userName: $userName, email: $email, password: $password) {
       token
       account {
         id
@@ -11,4 +11,4 @@ const registerMutation = gql`
   }
 `
 
- export default registerMutation
+export default registerMutation
