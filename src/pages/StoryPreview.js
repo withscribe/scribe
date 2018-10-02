f76501a9ba589642f7fc4a6ebcde8f70328e8d4b
Gerhard Knelsen <gerhard.l.k11@gmail.com>
2018-10-02 15:15:32 -0400

    added a simple Modal sheet to the StoryPreview Page
    no redirection yet.. cannot get the id from the clone response.
    
    May need to create a new Store to manage the state of the StoryPreview Page.

--- src/pages/StoryPreview.js
+++ src/pages/StoryPreview.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Modal from 'react-modal'
 import { inject, observer } from 'mobx-react'
 import { Link } from 'react-router-dom'
 
@@ -11,6 +12,17 @@ import Input, {
 @inject('storyStore', 'userStore')
 @observer
 class StoryPreview extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      modalIsOpen: false,
+    }
+
+    this.afterOpenModal = this.afterOpenModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+  }
+
   componentDidMount() {
     const { storyStore } = this.props
     const id = storyStore.selectedStory
@@ -21,10 +33,35 @@ class StoryPreview extends React.Component {
   cloneStory = (parentStoryId) => {
     const { storyStore, userStore } = this.props
     storyStore.clone(parentStoryId, userStore.me.id)
+      .then((res) => {
+        console.log(`CloneStory Response: ${res}`)
+      }).catch((err) => {
+        console.log(`CloneStory Error: ${err}`)
+      })
+    this.setState({ modalIsOpen: true })
+  }
+
+  afterOpenModal = () => {
+    // references are now sync'd and can be accessed.
+  }
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false })
+  }
+
+  viewStory = () => {
+    // redirect to Story preview with the cloned story id
+    console.log('Cloned Story Success Model: View')
+  }
+
+  editStory = () => {
+    // redirect to Story editor with the cloned story id
+    console.log('Cloned Story Success Model: Edit')
   }
 
   render() {
     const { storyStore: { story } } = this.props
+    const { modalIsOpen } = this.state
     return (
         <>
           {story && (
@@ -42,6 +79,19 @@ class StoryPreview extends React.Component {
               <Button onClick={() => this.cloneStory(story.id)}>Clone Story</Button>
             </>
           )}
+
+          <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            contentLabel="Clone Success Modal">
+
+            <h2>Story Cloned!</h2>
+            <button type="button" onClick={this.closeModal}>close</button>
+            <button type="button" onClick={this.viewStory}>View</button>
+            <button type="button" onClick={this.editStory}>Edit</button>
+          </Modal>
+          
       </>
     )
   }
