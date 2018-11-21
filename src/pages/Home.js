552f566f0e7cc525859869bf217b37dfdb51cd39
Evan Kysley <evan@kysley.com>
2018-11-20 19:23:48 -0500

    Change: Redesign Home with a bunch of temp divs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { inject, observer } from 'mobx-react'
 
 import Tab from 'Components/Tabs/Tab'
+import { TabList } from '_system/Tabs'
 import StoryCard from 'Components/StoryCard'
 import { HomeGrid } from '_system/Grid'
 import { TitleText } from '_system/Typography'
@@ -26,7 +27,6 @@ class Home extends React.Component {
     const { storyStore, history } = this.props
     return (
       <>
-        <TitleText>Discover</TitleText>
         <GhostWrapper isDoneRendering={storyStore.fetchingStories}>
           <HomeGrid>
             <GhostSmall style={{ backgroundColor: '#efefef' }} />
@@ -35,15 +35,36 @@ class Home extends React.Component {
             <GhostSmall style={{ backgroundColor: '#efefef' }} />
           </HomeGrid>
         </GhostWrapper>
-        {tabs.map((tab, index) => (
-          <Tab
-            key={tab}
-            id={tab}
-            onSelect={() => this.setState({ selectedIndex: index })}
-            isSelected={index === selectedIndex}>
-            {tab}
-          </Tab>
-        ))}
+        <div
+          style={{
+            width: '50vw',
+            display: 'flex',
+            alignSelf: 'center',
+            flexDirection: 'column',
+          }}>
+          <div
+            style={{
+              backgroundColor: '#e3c4ff',
+              borderRadius: '6px',
+              marginTop: '2em',
+              marginBottom: '2em',
+              padding: '2em',
+            }}>
+            <TitleText>Discover</TitleText>
+            Find new stories that are being published by other users
+          </div>
+          <TabList>
+            {tabs.map((tab, index) => (
+              <Tab
+                key={tab}
+                id={tab}
+                onSelect={() => this.setState({ selectedIndex: index })}
+                isSelected={index === selectedIndex}>
+                {tab}
+              </Tab>
+            ))}
+          </TabList>
+        </div>
         <HomeGrid>
           {!storyStore.fetchingStories && selectedIndex === 0
             ? <>
