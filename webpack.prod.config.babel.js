4a2d4d6d7250c5ec177e13d55e1609577018b21d
Evan Kysley <evan@kysley.com>
2019-01-05 19:31:05 -0500

    Chore: Manage prod bundling

--- webpack.prod.config.babel.js
+++ webpack.prod.config.babel.js
@@ -126,26 +126,14 @@ export default {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
-          parse: {
-            ecma: 8,
-          },
-          compress: {
-            ecma: 5,
-            warnings: false,
-            comparisons: false,
-            inline: 2,
-          },
-          mangle: {
-            safari10: true,
-          },
+          compress: true,
+          parallel: true,
+          sourceMap: false,
+          mangle: true,
           output: {
-            ecma: 5,
             comments: false,
-            ascii_only: true,
           },
         },
-        parallel: true,
-        sourceMap: false,
       }),
     ],
     runtimeChunk: false,
