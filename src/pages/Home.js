145709b59950ecf308652966eac0cbc2183641ec
Evan Kysley <evankysley@Evans-MacBook-Pro.local>
2019-01-11 20:07:06 -0500

    Chore: Linting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { inject, observer } from 'mobx-react'
 import { Link } from 'react-router-dom'
 
 import Tab from 'Components/Tabs/Tab'
+import Pagination from 'Components/Pagination'
 import StoryCard from 'Components/StoryCard'
 import CommunityCard from 'Components/CommunityCard'
 import { TabList } from 'System/Tabs'
@@ -48,31 +49,33 @@ class Home extends React.Component {
         </TabList>
         <HomeGrid>
           {!storyStore.fetchingStories && selectedIndex === 0
-            ? <>
-              {storyStore.nonClonedStories.map((story, idx) => {
-                if (idx === 3) {
+            ? (
+              <>
+                {storyStore.nonClonedStories.map((story, idx) => {
+                  if (idx === 3) {
+                    return (
+                      <Hero appearance="green" key="hero_2018aabda">
+                        <HeroPrimaryText>Feeling Creative?</HeroPrimaryText>
+                        <Link to="/story/create">
+                          <Button
+                            appearance="default"
+                            intent="success">
+                            Write your own Story
+                          </Button>
+                        </Link>
+                      </Hero>
+                    )
+                  }
                   return (
-                    <Hero appearance="green" key="hero_2018aabda">
-                      <HeroPrimaryText>Feeling Creative?</HeroPrimaryText>
-                      <Link to="/story/create">
-                        <Button
-                          appearance="default"
-                          intent="success">
-                          Write your own Story
-                        </Button>
-                      </Link>
-                    </Hero>
+                    /* we pass the history prop down to each card so we avoid a ton of
+                      unnecessary working..
+                      (not sure if it would even be a performance problem)
+                    */
+                    <StoryCard history={history} story={story} key={story.id} />
                   )
-                }
-                return (
-                  /* we pass the history prop down to each card so we avoid a ton of
-                    unnecessary working..
-                    (not sure if it would even be a performance problem)
-                  */
-                  <StoryCard history={history} story={story} key={story.id} />
-                )
-              })}
-            </>
+                })}
+              </>
+            )
             : (
               <GhostWrapper isDoneRendering={storyStore.fetchingStories}>
                 <HomeGrid>
@@ -87,18 +90,20 @@ class Home extends React.Component {
         </HomeGrid>
         <HomeGrid>
           {!communityStore.fetchingCommunities && selectedIndex === 1
-            ? <>
-              <Hero appearance="teal">
-                <HeroPrimaryText>Can't find a community?</HeroPrimaryText>
-                {/* <Link to="/community/create"> */}
+            ? (
+              <>
+                <Hero appearance="teal">
+                  <HeroPrimaryText>Can't find a community?</HeroPrimaryText>
+                  {/* <Link to="/community/create"> */}
                   {/* <Button appearance="default">Create it yourself</Button> */}
                   <HeroSpanText>Let Scribe know! We create user requested communities all the time.</HeroSpanText>
-                {/* </Link> */}
-              </Hero>
-              {communityStore.communities.map(community => (
-                <CommunityCard history={history} community={community} key={community.id} />
-              ))}
-            </>
+                  {/* </Link> */}
+                </Hero>
+                {communityStore.communities.map(community => (
+                  <CommunityCard history={history} community={community} key={community.id} />
+                ))}
+              </>
+            )
             : (
               <GhostWrapper isDoneRendering={communityStore.fetchingCommunities}>
                 <HomeGrid>
