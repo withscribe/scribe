ac82e47417f2a3eb906882bb0ef5cc19aef04ef9
Evan Kysley <evankysley@Evans-MacBook-Pro.local>
2019-01-21 16:50:54 -0500

    Fix: z-index bug with toast container

--- src/styled/Toast/ToastContainer.js
+++ src/styled/Toast/ToastContainer.js
@@ -4,15 +4,22 @@ const ToastContainer = styled.div`
   box-sizing: border-box;
   max-height: 100%;
   overflow-x: hidden;
-  overflow-y: hidden;
-  max-width: 560px;
-  padding: 1em;
-  margin: 0px auto;
+  overflow-y: auto;
+  pointer-events: auto;
   position: fixed;
-  top: 0;
-  right: 0;
+  top: 0px;
+  right: 0px;
   left: 0;
-  z-index: 1000;
+  width: 650px;
+  margin: 0 auto;
+  z-index: 100;
+
+  // fix so that the first toast doesn't appear flush with the top of the screen
+  >div {
+    :first-of-type {
+      padding-top: 1em;
+    }
+  }
 `
 
 export default ToastContainer
